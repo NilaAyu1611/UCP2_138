ca42335624ece073e52e775af79ca4049755aff6
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 17:07:48 +0700

    Membuat data class matakuliah dengan atributnya

---
+++ app/src/main/java/com/example/ucp2/Data/entity/matakuliah.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.Data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "matakuliah")
+data class matakuliah(
+    @PrimaryKey
+    val kode:String,
+    val nama: String,
+    val sks: String,
+    val semester: String,
+    val jenismk: String,
+    val dosenpengampu: String,
+)
