0fd124547c5438c9fe7a6d1b182a4b0eeb5f6f90
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-16 17:01:38 +0700

    Membuat atribut dari tabel dosen

--- app/src/main/java/com/example/ucp2/Data/entity/Dosen.kt
+++ app/src/main/java/com/example/ucp2/Data/entity/Dosen.kt
@@ -1,3 +1,12 @@
 package com.example.ucp2.Data.entity
 
-data class Dosen()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dosen")
+data class Dosen(
+    @PrimaryKey
+    val nidn: String,
+    val nama: String,
+    val jeniskelamin: String,
+)
