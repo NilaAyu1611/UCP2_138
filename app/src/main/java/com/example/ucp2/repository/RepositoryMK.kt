3594fc5abd42b221c83fd1fb37ecb7a0082bd004
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 23:22:26 +0700

    fungsi untuk menghapus data mk

--- app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
@@ -7,5 +7,6 @@ interface RepositoryMK {
     suspend fun insertMatakuliah(matakuliah: Matakuliah)
     fun getAllMatakuliah(): Flow<List<Matakuliah>>          //mendapatkan semua data dosen dlm bentuk aliran flow
     fun getMatakuliah(kode: String): Flow<Matakuliah>       //mengambil data dosen berdasarkan nama
+    suspend fun deleteMatakuliah(matakuliah: Matakuliah)    // menghapus data mk
 
 }
