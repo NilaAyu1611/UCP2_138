497e8cca71b41a6e2d37942f45cc23493c751eb1
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 21:03:25 +0700

    Fungsi deleteMatkul ini untuk menghapus data tertentu dr tabel matakuliah

--- app/src/main/java/com/example/ucp2/Data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/ucp2/Data/dao/MataKuliahDao.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2.Data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ucp2.Data.entity.Matakuliah
@@ -19,5 +20,6 @@ interface MataKuliahDao {
     @Query("select* from matakuliah where kode= :kode")
     fun getMatakuliah(kode: String): Flow<Matakuliah>
 
-
+    @Delete
+    suspend fun deleteMatakuliah(matakuliah: Matakuliah)
 }
