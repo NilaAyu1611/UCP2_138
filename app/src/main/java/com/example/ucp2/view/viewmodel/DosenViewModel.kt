f79808dae08c8f9d7b24b0c7a264bf2e088c9291
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 23:52:52 +0700

    Membuat class DosenViewModel

---
+++ app/src/main/java/com/example/ucp2/view/viewmodel/DosenViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.ucp2.view.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryDsn
+
+class DosenViewModel (private val repositoryDsn: RepositoryDsn): ViewModel(){
+
+
+
+}
+
+
