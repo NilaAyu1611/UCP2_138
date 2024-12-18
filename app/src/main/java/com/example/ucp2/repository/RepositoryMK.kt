8e33aced530ce295b38df614f76c370bc147b8aa
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 23:19:42 +0700

    fungsi untuk mengambil data mk berdasarkan kode

--- app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
@@ -6,5 +6,6 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryMK {
     suspend fun insertMatakuliah(matakuliah: Matakuliah)
     fun getAllMatakuliah(): Flow<List<Matakuliah>>          //mendapatkan semua data dosen dlm bentuk aliran flow
+    fun getMatakuliah(kode: String): Flow<Matakuliah>       //mengambil data dosen berdasarkan nama
 
 }
