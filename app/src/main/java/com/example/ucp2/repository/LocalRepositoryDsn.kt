a1e0e627e8d74a00bc0fc8fdce5e8f3954e457af
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 23:06:27 +0700

    Memanggil fungsi getAllDosen untuk mendapatkan semua data dosen

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
@@ -2,6 +2,7 @@ package com.example.ucp2.repository
 
 import com.example.ucp2.Data.dao.DosenDao
 import com.example.ucp2.Data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 //menghubungkan DosenDao dengan operasi yang didefinisikan dlaam RepositoryDsn
 class LocalRepositoryDsn (
@@ -10,6 +11,11 @@ class LocalRepositoryDsn (
     override suspend fun insertDosen(dosen: Dosen) {
         dosenDao.insertDosen(dosen)
     }
+
+    override fun getAllDosen(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+
 
 
 }
