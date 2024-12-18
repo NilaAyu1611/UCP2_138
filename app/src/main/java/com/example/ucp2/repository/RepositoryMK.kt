68896e90991a5684623bb427ed833db84027573a
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 23:23:58 +0700

    fungsi untuk memperbarui data mk di database

--- app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMK.kt
@@ -8,5 +8,6 @@ interface RepositoryMK {
     fun getAllMatakuliah(): Flow<List<Matakuliah>>          //mendapatkan semua data dosen dlm bentuk aliran flow
     fun getMatakuliah(kode: String): Flow<Matakuliah>       //mengambil data dosen berdasarkan nama
     suspend fun deleteMatakuliah(matakuliah: Matakuliah)    // menghapus data mk
+    suspend fun updateMatakuliah(matakuliah: Matakuliah)    // memperbarui data mk di db
 
 }
