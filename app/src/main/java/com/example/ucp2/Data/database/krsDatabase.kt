e643c89449b2782f827ef0e239191b3c1a488e70
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 21:46:18 +0700

    Mmeberikan akses ke operasi CR pada tabel Dosen

--- app/src/main/java/com/example/ucp2/Data/database/krsDatabase.kt
+++ app/src/main/java/com/example/ucp2/Data/database/krsDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2.Data.database
 
 import androidx.room.Database
+import com.example.ucp2.Data.dao.DosenDao
+import com.example.ucp2.Data.dao.MataKuliahDao
 import com.example.ucp2.Data.entity.Dosen
 import com.example.ucp2.Data.entity.Matakuliah
 
@@ -9,6 +11,10 @@ import com.example.ucp2.Data.entity.Matakuliah
     version = 1,
     exportSchema = false)
 abstract class krsDatabase(){
+
+    abstract fun dosenDao(): DosenDao       // Dao untuk mengakses data di tabel dosen
+
+
 
 
 }
