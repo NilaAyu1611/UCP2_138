935611a979ddfa082aed161881526374219cce7e
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-18 22:03:58 +0700

    Fungsi ini untuk mendapatkan instance dari krsDatabase

--- app/src/main/java/com/example/ucp2/Data/database/krsDatabase.kt
+++ app/src/main/java/com/example/ucp2/Data/database/krsDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.ucp2.Data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import com.example.ucp2.Data.dao.DosenDao
 import com.example.ucp2.Data.dao.MataKuliahDao
 import com.example.ucp2.Data.entity.Dosen
@@ -16,5 +18,20 @@ abstract class krsDatabase(){
 
     abstract fun mataKuliahDao(): MataKuliahDao     // Dao untuk mengakses data di tabel matakuliah
 
+    companion object{
+        @Volatile                   //memastikan bahwa nilai variabel instance selau sama di semua thread
+        private var Instance: krsDatabase? = null
+
+        fun getDatabase(context: Context): krsDatabase{
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    krsDatabase::class.java,        //class DB
+                    "krsDatabase"               // nama DB
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
 
 }
