c40fb8155cbedf12ad82d65fdaf88a12c8450258
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-19 00:25:28 +0700

    fungsi untuk menyimpan data, jika terjadi kesalahan / tidak maka akan menampilakn pesan

--- app/src/main/java/com/example/ucp2/view/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/view/viewmodel/DosenViewModel.kt
@@ -4,8 +4,10 @@ package com.example.ucp2.view.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.Data.entity.Dosen
 import com.example.ucp2.repository.RepositoryDsn
+import kotlinx.coroutines.launch
 
 class DosenViewModel (private val repositoryDsn: RepositoryDsn): ViewModel(){
 
@@ -28,7 +30,31 @@ class DosenViewModel (private val repositoryDsn: RepositoryDsn): ViewModel(){
         return errorState.isValid()
     }
 
+    fun saveData(){
+        val currentEvent = uiState.dosenEvent
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    repositoryDsn.insertDosen((currentEvent.toDoseenEntity()))
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        dosenEvent = DosenEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                }
+                catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
 
+        }else{
+            uiState = uiState.copy(
+                snackBarMessage = "Input tidak valid, periksa kembali data Anda"
+            )
+        }
+    }
 
 }
 
