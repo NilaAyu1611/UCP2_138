49b8bcd54e104ea8aa6f79f2e015220df1993eb5
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 06:36:00 +0700

    Membuat destinasi InsertDsnView.kt untuk ruiting halamanya

---
+++ app/src/main/java/com/example/ucp2/view/screen/InsertDsnView.kt
@@ -0,0 +1,14 @@
+package com.example.ucp2.view.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.navigation.AlamatNavigasi
+import com.example.ucp2.view.viewmodel.DosenViewModel
+
+@Preview(showBackground = true)
+object DestinasiInsert : AlamatNavigasi{
+    override val route: String = "insert_dsn"
+}
+
