eae1b8f7e25db89114295f9ec57cb624067099f2
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 06:45:00 +0700

    Membuat variabel utk mengelola UI secara deklaratif

--- app/src/main/java/com/example/ucp2/view/screen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/view/screen/InsertDsnView.kt
@@ -1,6 +1,9 @@
 package com.example.ucp2.view.screen
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -19,5 +22,10 @@ fun InsertDsnView(
     modifier: Modifier = Modifier,
     viewModel: DosenViewModel = viewModel(factory = PenyedeiaViewModel.Factory)
 ){
+    val uiState = viewModel.uiState         //mengambil ui state dari VM
+    val snackbarHostState = remember {SnackbarHostState()}      // Snacbar state/menampilkan pesan kpd user terkait ui
+    val corutineScope = rememberCoroutineScope()
+
+
 
 }
