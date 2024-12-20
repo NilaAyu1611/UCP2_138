9b1dea3e7738d6e3b33c24449d1c8fd4f38509c8
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-21 01:39:16 +0700

    Penambahan getalldosennames pd DosenDao

--- app/src/main/java/com/example/ucp2/Data/dao/DosenDao.kt
+++ app/src/main/java/com/example/ucp2/Data/dao/DosenDao.kt
@@ -11,6 +11,10 @@ import kotlinx.coroutines.flow.Flow
 interface DosenDao {
     @Query("select* from dosen")
     fun getAllDosen(): Flow<List<Dosen>>
+
+    @Query("SELECT nama FROM dosen")
+    suspend fun getAllDosenNames(): List<String> // Mengembalikan daftar nama dosen
+
     @Insert
     suspend fun insertDosen(
         dosen: Dosen
