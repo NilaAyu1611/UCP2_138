c5f50d717c66ccc831326a1241db5b7a4eef7699
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 16:58:54 +0700

    Membuat fungsi BodyDetailMK dan propertinya

--- app/src/main/java/com/example/ucp2/view/matakuliah/DetailMKView.kt
+++ app/src/main/java/com/example/ucp2/view/matakuliah/DetailMKView.kt
@@ -1,8 +1,11 @@
 package com.example.ucp2.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -10,11 +13,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.view.viewmodel.Matakuliah.DetailMKViewModel
+import com.example.ucp2.view.viewmodel.Matakuliah.DetailMKlUiState
 import com.example.ucp2.view.viewmodel.PenyediaViewModel
 
 @Composable
@@ -58,5 +67,25 @@ fun DetailMKView(
                 onDeleteClick()
             }
         )
+    }
+}
+
+@Composable
+fun BodyDetailMK(
+    modifier: Modifier = Modifier,
+    detailMKUiState: DetailMKlUiState = DetailMKlUiState(),
+    onDeleteClick: () -> Unit = {}
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailMKUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
     }
 }
