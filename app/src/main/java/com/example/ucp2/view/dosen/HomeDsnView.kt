430b418ad7be9beea1bf6a0601d27a05dae9e520
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 13:16:08 +0700

    Membuat fungsi HomeDsnView, memanggil dari funsi BodyHomeDsnView

--- app/src/main/java/com/example/ucp2/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/view/dosen/HomeDsnView.kt
@@ -1,2 +1,60 @@
 package com.example.ucp2.view.dosen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.view.viewmodel.Dosen.HomeDsnViewModel
+import com.example.ucp2.view.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeDsnView(
+    viewModel: HomeDsnViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDsn: () -> Unit = {},
+    onDetailClik: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = false,
+                onBack = {}
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen",
+                )
+            }
+        }
+
+    ){innerPadding ->
+        val homeUiState by viewModel.homeUIState.collectAsState()
+
+        BodyHomeDsnView(
+            homeUiState = homeUiState,
+            onclick = {
+                onDetailClik
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
