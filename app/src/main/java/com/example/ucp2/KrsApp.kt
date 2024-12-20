8f0dba1cbec41a0ec1210bec0aea1ebec7813ccf
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 08:47:34 +0700

    Membuat class ContainerApp dari dosen dan mk

--- app/src/main/java/com/example/ucp2/KrsApp.kt
+++ app/src/main/java/com/example/ucp2/KrsApp.kt
@@ -1,6 +1,7 @@
 package com.example.ucp2
 
 import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
 
 class KrsApp : Application() {
                                             // fungsinya untuk menyimpan instance containerapp
