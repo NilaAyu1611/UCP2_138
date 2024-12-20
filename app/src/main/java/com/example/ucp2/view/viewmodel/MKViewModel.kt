d598842ca7357891a8b51a8d0ccb3ece213c4889
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 10:27:53 +0700

    Perbaikan untuk FormErrorState menjadi FormErrorStateMK

--- app/src/main/java/com/example/ucp2/view/viewmodel/MKViewModel.kt
+++ app/src/main/java/com/example/ucp2/view/viewmodel/MKViewModel.kt
@@ -1,19 +1,46 @@
 package com.example.ucp2.view.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import androidx.lifecycle.ViewModel
 import com.example.ucp2.Data.entity.Matakuliah
+import com.example.ucp2.repository.RepositoryMK
 
+
+class MKViewModel (private val repositoryMK: RepositoryMK) : ViewModel(){
+
+    var uiState by mutableStateOf(MKUIState())
+
+    fun update(matakuliahEvent: MatakuliahEvent){
+        uiState = uiState.copy(
+            matakuliahEvent = matakuliahEvent,
+        )
+
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.matakuliahEvent
+        val errorState = FormErrorStateMK (
+            kode = if (event.kode.isNotEmpty()) null else "Kode tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+
+
+
+        )
+    }
+    
+}
 
 
 
 //untuk merubah state/perubahan
 data class MKUIState(
     val matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
-    val isEntryValid: FormErrorState = FormErrorState(),
+    val isEntryValid: FormErrorStateMK = FormErrorStateMK(),
     val snackBarMessage: String? = null,
 )
 
-data class FormErrorState(
+data class FormErrorStateMK(
     val kode: String? = null,
     val nama: String? = null,
     val sks: String? = null,
