dd8c581f06aea1bbc0d8c3dfcedc68e8de970827
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 14:03:42 +0700

    Membuat fungsi BodyDetailDsn dengan layoutnya

--- app/src/main/java/com/example/ucp2/view/dosen/DetailDsnView.kt
+++ app/src/main/java/com/example/ucp2/view/dosen/DetailDsnView.kt
@@ -1,14 +1,24 @@
 package com.example.ucp2.view.dosen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.view.viewmodel.Dosen.DetailDsnViewModel
+import com.example.ucp2.view.viewmodel.Dosen.DetailUiState
+import com.example.ucp2.view.viewmodel.Dosen.toDosenEntity
 import com.example.ucp2.view.viewmodel.PenyediaViewModel
 
 @Composable
@@ -33,5 +43,47 @@ fun DetailDsnView(
             detailUiState = detailUiState
         )
 
+    }
+}
+
+@Composable
+fun BodyDetailDsn(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState()
+) {
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDsn(
+                    dosen = detailUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
     }
 }
