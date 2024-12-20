e8da2731d1c85192bc9bc273aaf3982e221bf93c
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 17:36:13 +0700

    Menampilkan sebuah snacbar ketika ada perubahan pd properti

--- app/src/main/java/com/example/ucp2/view/matakuliah/UpdateMKView.kt
+++ app/src/main/java/com/example/ucp2/view/matakuliah/UpdateMKView.kt
@@ -1,13 +1,16 @@
 package com.example.ucp2.view.matakuliah
 
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.view.viewmodel.Matakuliah.UpdateMKViewModel
 import com.example.ucp2.view.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun UpdateMKView(
@@ -21,5 +24,16 @@ fun UpdateMKView(
     val snackbarHostState = remember  { SnackbarHostState() }        // Snackbar state
     val coroutineScope = rememberCoroutineScope()
 
-
+    // Observasi perubahan snackbarMessage
+    LaunchedEffect (uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
 }
