23e059f89a185993ea6c3dae4eb5dc62ef4a7d59
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 08:30:30 +0700

    Membuat object PenyediaViewModelvuntuk dosen

---
+++ app/src/main/java/com/example/ucp2/view/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.ucp2.view.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            HomeDsnViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailDsnViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+
+    }
+}
