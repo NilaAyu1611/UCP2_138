3a9db52d56a039b79066f73fabd2ca6adf773145
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 08:23:01 +0700

    Membuat class DetailUiState dari tebel dosen

--- app/src/main/java/com/example/ucp2/view/viewmodel/DetailDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2/view/viewmodel/DetailDsnViewModel.kt
@@ -49,3 +49,16 @@ class DetailDsnViewModel (
         )
 }
 
+class DetailUiState(
+    val detailUiEvent: DosenEvent = DosenEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == DosenEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != DosenEvent()
+}
+
