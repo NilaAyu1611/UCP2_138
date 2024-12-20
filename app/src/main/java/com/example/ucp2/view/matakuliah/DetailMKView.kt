4144215be9088dd459a388017934baf0ce628b51
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 17:12:29 +0700

    Membuat fungsi BodyDetailMK, pada detailmkuistate (delete)

--- app/src/main/java/com/example/ucp2/view/matakuliah/DetailMKView.kt
+++ app/src/main/java/com/example/ucp2/view/matakuliah/DetailMKView.kt
@@ -1,15 +1,20 @@
 package com.example.ucp2.view.matakuliah
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,8 +27,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.costumwidget.TopAppBar
+import com.example.ucp2.view.dosen.ItemDetailDsn
 import com.example.ucp2.view.viewmodel.Matakuliah.DetailMKViewModel
 import com.example.ucp2.view.viewmodel.Matakuliah.DetailMKlUiState
+import com.example.ucp2.view.viewmodel.Matakuliah.toMatakuliahEntity
 import com.example.ucp2.view.viewmodel.PenyediaViewModel
 
 @Composable
@@ -84,6 +91,37 @@ fun BodyDetailMK(
                 contentAlignment = Alignment.Center
             ) {
                 CircularProgressIndicator()
+            }
+        }
+        detailMKUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailMK(
+                    matakuliah = detailMKUiState.detailUiEvent.toMatakuliahEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button (
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
             }
         }
 
