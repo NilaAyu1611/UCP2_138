54954cea22d05021836c2e2dae4e9a159703bcfb
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 17:45:06 +0700

    Menampilkan info topappbar, memvalidasi & simpan perubahan MK

--- app/src/main/java/com/example/ucp2/view/matakuliah/UpdateMKView.kt
+++ app/src/main/java/com/example/ucp2/view/matakuliah/UpdateMKView.kt
@@ -1,16 +1,27 @@
 package com.example.ucp2.view.matakuliah
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.view.viewmodel.Matakuliah.UpdateMKViewModel
 import com.example.ucp2.view.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMKView(
@@ -34,6 +45,44 @@ fun UpdateMKView(
                 )
                 viewModel.resetSnackBarMessage()
             }
+        }
+    }
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },     // Tempatkan Snackbar di Scaffold
+        topBar = {
+            TopAppBar(
+                judul = "Edit Mata Kuliah",
+                showBackButton = true,
+                onBack = onBack,
+            )
+
+        }
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Isi Body
+            InsertBodyMk (
+                uiState = uiState,
+                onValueChange = { updatedEvent ->
+                    viewModel.updateState(updatedEvent)         // Update state di ViewModel
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate()                    // Navigasi di main thread
+                            }
+                        }
+                    }
+                }
+            )
         }
     }
 }
