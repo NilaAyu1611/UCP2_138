5802d2184352c9fca9bbad259f6b791bac32a855
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 09:37:15 +0700

    Membuat data class MatakuliahEvent

---
+++ app/src/main/java/com/example/ucp2/view/viewmodel/MKViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.view.viewmodel
+
+
+
+
+
+//data class variabel yang menyimpan data input form
+data class MatakuliahEvent(
+    val kode:String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val jenismk: String = "",
+    val dosenpengampu: String = "",
+)
