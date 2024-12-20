f9270ac052d3e7d5fdde38392ecf22c5380aeb0b
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 08:47:49 +0700

    Membuat class ContainerApp dari dosen dan mk

---
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -0,0 +1,25 @@
+package com.example.ucp2.dependenciesinjection
+
+import android.content.Context
+import com.example.ucp2.Data.database.krsDatabase
+import com.example.ucp2.repository.LocalRepositoryDsn
+import com.example.ucp2.repository.LocalRepositoryMK
+import com.example.ucp2.repository.RepositoryDsn
+import com.example.ucp2.repository.RepositoryMK
+
+interface InterfaceContainerApp{
+    val repositoryDsn: RepositoryDsn
+    val repositoryMK: RepositoryMK
+}
+
+class ContainerApp (private val context: Context): InterfaceContainerApp{
+    override val repositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(krsDatabase.getDatabase(context).dosenDao())
+    }
+
+    override val repositoryMK: RepositoryMK by lazy {
+        LocalRepositoryMK(krsDatabase.getDatabase(context).mataKuliahDao())
+    }
+
+
+}
