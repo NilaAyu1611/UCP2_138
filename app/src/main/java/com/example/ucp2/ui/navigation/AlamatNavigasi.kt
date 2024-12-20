a6d57bd6edc2c25051c948b861cc8192c605a2cb
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-20 22:28:55 +0700

    Membuat navigasi untuk home utama

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -3,3 +3,9 @@ package com.example.ucp2.ui.navigation
 interface AlamatNavigasi{
     val route: String
 }
+
+//Navigasi untuk Home Utama
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
+
