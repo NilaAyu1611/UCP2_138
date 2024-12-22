7ddb6b14157da78d0621a42c196bd167e892b2d8
NilaAyu1611 <nilaayucw@gmail.com>
2024-12-22 14:35:25 +0700

    Styleing pd TopAppBar.kt

--- app/src/main/java/com/example/ucp2/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/costumwidget/TopAppBar.kt
@@ -1,15 +1,24 @@
 package com.example.ucp2.ui.costumwidget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,24 +28,42 @@ fun TopAppBar(
     onBack: () -> Unit,
     showBackButton: Boolean = true,
     judul: String,
+    titleColor: Color = Color.White, // Default warna putih
+    iconColor: Color = Color.White
 ){
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF25549d), // Biru tua
+                        Color(0xFF939caa)  // Hijau terang
+                    )
+                ),
+
+            )
+            .padding(top = 16.dp),
         contentAlignment = Alignment.Center             // Pastikan konten di tengah
     )
+
     {
+
         if (showBackButton) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                TextButton(
+                IconButton (
                     onClick = onBack,
                     modifier = Modifier.align(Alignment.CenterVertically)
                 ) {
-                    Text("Kembali")
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "kembali",
+                        tint = iconColor
+                    )
+
                 }
                 Spacer(modifier = Modifier.weight(2f))
             }
@@ -47,6 +74,7 @@ fun TopAppBar(
             text = judul,
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold,
+            color = titleColor,
             modifier = Modifier.align(Alignment.Center)
         )
 
